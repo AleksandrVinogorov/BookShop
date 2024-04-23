bb562ed223d13542b26b909a3d1c583f72b3bb88
AleksandrVinogorov <119435769+AleksandrVinogorov@users.noreply.github.com>
2024-04-23 17:42:24 +0300

    Update loadBooks.js

--- src/card/loadBooks.js
+++ src/card/loadBooks.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 let LoadBooks = ({search, startIndex, setData, resultsLimit}) => {
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&startIndex=${startIndex}&maxResults=${resultsLimit}&langRestrict=en&key=${AIzaSyCRnqBwHe_-hfyGAiE2oN1ALD4oxt1CUgA)`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&startIndex=${startIndex}&maxResults=${resultsLimit}&langRestrict=en&key=${AIzaSyCRnqBwHe_-hfyGAiE2oN1ALD4oxt1CUgA}`
+  );
   .then(res => setData(res.data.items)) // заменяем старые данные на новые при каждой загрузке
   .catch(err => console.log(err))
 }
